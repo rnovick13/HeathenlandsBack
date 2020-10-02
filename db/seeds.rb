673d7a4d4baebbe412d04c97e88c6cf81c846f23
rnovick13 <rnovick13@gmail.com>
2020-10-01 21:54:11 -0400

    start Malkine seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 echante = Species.create(name: "Echanté", att_inc: "4 points to be put into any attributes at the player's discretion" , speed: 30)
 
-protherian = Species.create(name: "Protherian", att_inc:)
+protherian = Species.create(name: "Protherian", att_inc: "TBD" , speed: 30)
+
+malkine = Species.create(name: "Malkine", att_inc: "tbd" , speed: 30)
