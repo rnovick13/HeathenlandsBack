1aaed2e3e7a739ae669f0c6ccfc3845298d3be85
rnovick13 <rnovick13@gmail.com>
2020-10-01 21:57:44 -0400

    start Viatalen seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 echante = Species.create(name: "Echanté", att_inc: "4 points to be put into any attributes at the player's discretion" , speed: 30)
 
-protherian = Species.create(name: "Protherian", att_inc: "TBD" , speed: 30)
-
-malkine = Species.create(name: "Malkine", att_inc: "TBD" , speed: 30)
+enffe = Species.create(name: "Enffé", att_inc: "TBD" , speed: 30)
 
 fey = Species.create(name: "Fey", att_inc: "TBD" , speed: 30)
 
-enffe = Species.create(name: "Enffé", att_inc: "TBD" , speed: 30)
+malkine = Species.create(name: "Malkine", att_inc: "TBD" , speed: 30)
+
+protherian = Species.create(name: "Protherian", att_inc: "TBD" , speed: 30)
+
+viatalen = Species.create(name: "Viatalen", att_inc: "TBD" , speed: 30)
