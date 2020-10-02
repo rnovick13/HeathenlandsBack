9663ec00e7701c00cb1f8c31c5ecad256b3ccffa
rnovick13 <rnovick13@gmail.com>
2020-10-01 21:51:38 -0400

    start Protherian seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
 echante = Species.create(name: "Echanté", att_inc: "4 points to be put into any attributes at the player's discretion" , speed: 30)
+
+protherian = Species.create(name: "Protherian", att_inc:)
